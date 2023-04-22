2cca04d53c5f1ed976ff86ac93f8013cb1c40c55
india@1410 <prakashsingh20497@gmail.com>
2023-04-22 14:14:54 +0530

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const jsonServer = require("json-server");
+const router=jsonServer.router("db.json")
+
+const server = jsonServer.create();
+
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use("/api", router);
+server.listen(process.env.PORT || 5000, () => {
+  console.log("JSON Server is running");
+});
